88cd70c52b8108e4a57b9690e195a842a47cce5f
Tupui <23188539+tupui@users.noreply.github.com>
2026-05-15 21:38:08 +0200

    Fix branch name

--- .radicle/ci.sh
+++ .radicle/ci.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
-rad_ci_branch=rad/ci
+rad_ci_branch=rad-ci
 rid=$(rad inspect --rid)
 node=${RADICLE_LOG_NODE:-iris.radicle.xyz}
 sha=$(git rev-parse HEAD)
